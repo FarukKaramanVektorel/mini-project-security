833fdfe78bf9c20e659be38996a54e92e79d0c25
ÖĞRETMEN-PC <RETMEN-PC@host.docker.internal>
2026-02-15 13:35:23 +0300

    second commit

--- src/main/java/com/personel/personel/repository/UserRepository.java
+++ src/main/java/com/personel/personel/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.personel.personel.repository;
 
 import java.util.Optional;
 
-import org.springframework.boot.security.autoconfigure.SecurityProperties.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.personel.personel.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	
